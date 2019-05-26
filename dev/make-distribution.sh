c6deadd446cd4a599d389d1408737605047a9d5c
Kay Marquardt (Gnadelwartz) <kay@rrr.de>
2019-05-26 16:55:02 +0200

    make DIST and STANDALONE uppercase

--- dev/make-distribution.sh
+++ dev/make-distribution.sh
@@ -2,7 +2,7 @@
 # file: make-distribution.sh
 # creates files and arcchives to dirtribute bashbot
 #
-#### $$VERSION$$ v0.90-dev2-0-gec85636
+#### $$VERSION$$ v0.90-dev2-1-g981c0ee
 
 # magic to ensure that we're always inside the root of our application,
 # no matter from which directory we'll run script
@@ -16,8 +16,8 @@ fi
 VERSION="$(git describe --tags | sed -e 's/-[0-9].*//' -e 's/v//')"
 
 DISTNAME="telegram-bot-bash"
-DISTDIR="./dist/${DISTNAME}" 
-DISTFILES="bashbot.rc  bashbot.sh  commands.sh  mycommands.sh doc  examples modules LICENSE  README.md  README.txt README.html"
+DISTDIR="./DIST/${DISTNAME}" 
+DISTFILES="bashbot.rc  bashbot.sh  commands.sh  mycommands.sh doc  examples modules addons LICENSE  README.md  README.txt README.html"
 
 # run tests first!
 
--- dev/make-standalone.sh
+++ dev/make-standalone.sh
@@ -5,7 +5,7 @@
 # If you your bot is finished you can use make-standalone.sh to create the
 # the old all-in-one bashbot:  bashbot.sh and commands.sh only!
 #
-#### $$VERSION$$ v0.90-dev2-0-gec85636
+#### $$VERSION$$ v0.90-dev2-1-g981c0ee
 
 # magic to ensure that we're always inside the root of our application,
 # no matter from which directory we'll run script
@@ -17,7 +17,7 @@ else
 fi
 
 #DISTNAME="telegram-bot-bash"
-DISTDIR="./standalone/${DISTNAME}" 
+DISTDIR="./STANDALONE/${DISTNAME}" 
 DISTFILES="bashbot.sh  commands.sh  mycommands.sh modules LICENSE README.txt token count botacl botadmin"
 
 # run tests first!
