74e0aa36a04d4388e65e5e6d54d4f468ba017827
Kay Marquardt (Gnadelwartz) <kay@rrr.de>
2020-12-13 15:49:16 +0100

    emergency fix: remove old workaround stopping interactive to work

--- modules/jsonDB.sh
+++ modules/jsonDB.sh
@@ -332,7 +332,7 @@ function jssh_updateArray_async() {
 # $1 ARRAY name, must be declared with "declare -A ARRAY" before calling
 Json2Array() {
 	# shellcheck disable=SC1091,SC1090
-	[ -z "$1" ] || source <( printf "$1"'=( %s )' "$(sed -E -n -e ':x /"text"\]/ { N; s/([^"])\n/\1\\n/g ; tx }' -e '/\["[-0-9a-zA-Z_,."]+"\]\+*\t/ s/\t/=/gp' -e 's/=(true|false)/="\1"/')" )
+	[ -z "$1" ] || source <( printf "$1"'=( %s )' "$(sed -E -n -e '/\["[-0-9a-zA-Z_,."]+"\]\+*\t/ s/\t/=/gp' -e 's/=(true|false)/="\1"/')" )
 }
 # get Config Key from jssh file without jsshDB
 # output ARRAY as JSON.sh style data
