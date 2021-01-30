c0f1af529e089b29f673fea6d1fb28d9b1fdcfa2
Kay Marquardt (Gnadelwartz) <kay@rrr.de>
2021-01-30 20:18:54 +0100

    bin: process_update.sh  webhook script

---
+++ bin/process_update.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# shellcheck disable=SC1090,SC2034,SC2059
+#===============================================================================
+#
+#          FILE: bin/process_update.sh
+#
+USAGE='process_update.sh [-h|--help] [debug] [<file]'
+# 
+#   DESCRIPTION: processes ONE telegram update read from stdin, e.g. form file or webhook
+#                 
+#                -h - display short help
+#                --help -  this help
+#
+#	LICENSE: WTFPLv2 http://www.wtfpl.net/txt/copying/
+#        AUTHOR: KayM (gnadelwartz), kay@rrr.de
+#       CREATED: 30.01.2021 19:14
+#
+#### $$VERSION$$ v1.40-dev-9-g1e49f6c
+#===============================================================================
+
+####
+# parse args
+COMMAND="process_update"
+
+# set bashbot environment
+source "${0%/*}/bashbot_env.inc.sh" "debug" # debug
+print_help "${1:-nix}"
+
+
+####
+# ready, do stuff here -----
+
+# read json from stdin and convert update format
+json='{"result": {"0":'"$(cat)"'}}'
+UPDATE="$(${JSONSHFILE} -b -n <<<"${json}")"
+
+# assign to bashbot ARRAY
+Json2Array 'UPD' <<<"${UPDATE}" 
+
+# process telegram update
+"${COMMAND}" "0" "$1" 
+
